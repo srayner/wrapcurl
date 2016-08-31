756061171015e800b2e0406330ae0057f591a5cc
Steve Rayner <srayner02@gmail.com>
2016-08-31 11:22:48 +0100

    Fixed bug in example.

--- src/Examples/Example.php
+++ src/Examples/Example.php
@@ -2,11 +2,11 @@
 
 use WrapCurl\Request;
 
-// TODO: register an autoloader.
+include __DIR__ . '/../../vendor/autoload.php';
 
 $url = 'www.google.com';
 $request = new Request($url);
 $response = $request->exec();
-file_put_contents($response);
+file_put_contents(__DIR__ . '/example.txt', $response);
 
 
